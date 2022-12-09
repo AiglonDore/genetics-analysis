19fae46eaa23940aecfd65cabd3df736284b18ed
Thomas Roiseux <thomas.roiseux@outlook.com>
2022-12-09 19:01:11 +0100

    Save

--- Group_Lasso.R
+++ Group_Lasso.R
@@ -2,6 +2,13 @@ library(gglasso)
 load("project_park.RData")
 
 df <- as.data.frame(Xmat)
+for (x in 1:nrow(df)) {
+    for (y in 1:ncol(df)){
+        if (is.na(df[x,y])) {
+            df[x,y] = -1
+        }
+    }
+}
 df$Y <- pheno.df$Protein.content
 rm(Xmat, geno.df, pheno.df)
 gc()
@@ -17,4 +24,27 @@ rm(i, reg)
 gc()
 
 absBetas <- abs(betas)
-sortedBetas <- sort(x = absBetas, index.return = TRUE, decreasing = TRUE)
+sortedIndex <- order(x = absBetas, decreasing = TRUE)
+sortedBetas <- c()
+for (x in sortedIndex){
+    sortedBetas <- append(sortedBetas, betas[x])
+}
+rm(x, absBetas)
+
+library(dplyr)
+
+variables <- df[sortedIndex[1:10000]]
+
+variables$Y <- df$Y
+variables <- na.omit(variables)
+
+tvariables <- t(variables)
+kmeans.res <- kmeans(x = tvariables[-c(10001), ], centers = 50, iter.max = 100)
+gglasso.res <- gglasso(as.matrix(variables[-c(10001)]), variables[, 10001], group = kmeans.res$cluster)
+plot(gglasso.res)
+
+library(glmnet)
+lambda <- cv.gglasso(as.matrix(variables[-c(10001)]), variables[, 10001], group = kmeans.res$cluster, nfolds = 10)
+lambda.min <- lambda$lambda.min
+
+gglasso.opt <- gglasso(as.matrix(variables[-c(10001)]), variables[, 10001], group = kmeans.res$cluster, lambda = lambda.min)
